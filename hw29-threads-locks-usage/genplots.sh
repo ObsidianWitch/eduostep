34e08a5ece7a5fb1eae3a47aa30726eb6bfb85e2
Obsidienne <obsidienne@protonmail.com>
2021-07-07 22:51:04 +0200

    hw29: fix genplots in & out filenames

--- hw29-threads-locks-usage/genplots.sh
+++ hw29-threads-locks-usage/genplots.sh
@@ -3,26 +3,26 @@
 set -o errexit -o nounset
 
 time {
-    for program in simple_counter sloppy_counter; do
+    for program in counter_simple counter_sloppy; do
     for nthreads in {1..4}; do
     for experiment in {1..100}; do
         "out/$program" "$nthreads" 1000000
-    done; done; done | ./plot_xthreads_ytime.py > counters_plot.png
+    done; done; done | ./plot_xthreads_ytime.py > plot_counters.png
 }
 
 time {
     for i in {0..10}; do
         threshold="$((2**i))"
         for experiment in {1..100}; do
-            out/sloppy_counter 4 1000000 "$threshold"
+            out/counter_sloppy 4 1000000 "$threshold"
         done
-    done | ./plot_xthreshold_ytime.py > threshold_plot.png
+    done | ./plot_xthreshold_ytime.py > plot_threshold.png
 }
 
 time {
-    for program in simple_list hoh_list; do
+    for program in list_simple list_hoh; do
     for nthreads in {1..4}; do
     for experiment in {1..100}; do
         "out/$program" "$nthreads" 1000000
-    done; done; done | ./plot_xthreads_ytime.py > lists_plot.png
+    done; done; done | ./plot_xthreads_ytime.py > plot_lists.png
 }
