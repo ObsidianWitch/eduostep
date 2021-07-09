dbae67be487e6b22dc589c78ae9b833080a65da6
Obsidienne <obsidienne@protonmail.com>
2021-07-09 07:43:11 +0200

    hw29: genplots progress

--- hw29-threads-locks-usage/genplots.sh
+++ hw29-threads-locks-usage/genplots.sh
@@ -6,6 +6,7 @@ time {
     for program in counter_simple counter_sloppy; do
     for nthreads in {1..4}; do
     for experiment in {1..100}; do
+        echo "$program $nthreads $experiment" >&2
         "out/$program" "$nthreads" 1000000
     done; done; done | ./plot_xthreads_ytime.py > plot_counters.png
 }
@@ -14,6 +15,7 @@ time {
     for i in {0..10}; do
         threshold="$((2**i))"
         for experiment in {1..100}; do
+            echo "$threshold $experiment" >&2
             out/counter_sloppy 4 1000000 "$threshold"
         done
     done | ./plot_xthreshold_ytime.py > plot_threshold.png
@@ -23,6 +25,7 @@ time {
     for program in list_simple list_hoh; do
     for nthreads in {1..4}; do
     for experiment in {1..100}; do
+        echo "$program $nthreads $experiment" >&2
         "out/$program" "$nthreads" 1000000
     done; done; done | ./plot_xthreads_ytime.py > plot_lists.png
 }
@@ -31,6 +34,7 @@ time {
     for program in bst_simple bst_better; do
     for nthreads in {1..4}; do
     for experiment in {1..100}; do
+        echo "$program $nthreads $experiment" >&2
         "out/$program" "$nthreads" 10000
     done; done; done | ./plot_xthreads_ytime.py > plot_bst.png
 }
