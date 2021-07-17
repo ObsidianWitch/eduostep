3e25b6c91f00217799cee4f50635b11f885bd278
Obsidienne <obsidienne@protonmail.com>
2021-07-17 16:21:58 +0200

    hw31: q5

---
+++ hw31-threads-sema/src/reader-writer-nostarve.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <semaphore>
+#include <chrono>
+
+class RWLock {
+public:
+    RWLock(): read_sem_(1), write_sem_(1), order_sem_(1), readers_(0) {}
+
+    void read_lock() {
+        order_sem_.acquire();
+        read_sem_.acquire();
+        readers_++;
+        if (readers_ == 1) {
+            write_sem_.acquire();
+        }
+        read_sem_.release();
+        order_sem_.release();
+    }
+
+    void read_unlock() {
+        read_sem_.acquire();
+        readers_--;
+        if (readers_ == 0) {
+            write_sem_.release();
+        }
+        read_sem_.release();
+    }
+
+    void write_lock() {
+        order_sem_.acquire();
+        write_sem_.acquire();
+        order_sem_.release();
+    }
+
+    void write_unlock() {
+        write_sem_.release();
+    }
+private:
+    std::counting_semaphore<1> read_sem_;
+    std::counting_semaphore<1> write_sem_;
+    std::counting_semaphore<1> order_sem_;
+    int readers_;
+};
+
+int value = 0;
+RWLock lock;
+
+void reader(int nloops) {
+    for (int i = 0; i < nloops; ++i) {
+        lock.read_lock();
+        std::cout << "r" << value << std::endl;
+        lock.read_unlock();
+    }
+}
+
+void writer(int nloops) {
+    for (int i = 0; i < nloops; ++i) {
+        lock.write_lock();
+        value++;
+        std::cout << "w" << value << std::endl;
+        lock.write_unlock();
+    }
+}
+
+int main(int argc, char *argv[]) {
+    std::cout << "parent: begin" << std::endl;
+    if (argc != 4) {
+        std::cerr << "usage: " << argv[0] << " <nreaders> <nwriters> <nloops>"
+                  << std::endl;
+        return 1;
+    }
+    auto nreaders = std::stoi(argv[1]);
+    auto nwriters = std::stoi(argv[2]);
+    auto nloops = std::stoi(argv[3]);
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < nreaders; ++i) {
+        threads.emplace_back(reader, nloops);
+    }
+    for (int i = 0; i < nwriters; ++i) {
+        threads.emplace_back(writer, nloops);
+    }
+    for (auto &thread : threads) {
+        thread.join();
+    }
+
+    std::cout << "parent: end: " << value << std::endl;
+    return 0;
+}
