52009d570c8ebfdf7b897bd0fcc759bd98b5a707
Obsidienne <obsidienne@protonmail.com>
2021-07-22 18:11:31 +0200

    hw33: 404 response

--- hw33-threads-events/src/main.rs
+++ hw33-threads-events/src/main.rs
@@ -4,24 +4,37 @@ use std::net::{TcpListener, TcpStream};
 // ref: https://doc.rust-lang.org/book/ch20-00-final-project-a-web-server.html
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:0").unwrap();
-    println!("TCP listener on {}", listener.local_addr().unwrap());
+    println!("Server: http://{}/", listener.local_addr().unwrap());
 
     for stream in listener.incoming() {
-        handle_connection(stream.unwrap());
+        handle_http_request(stream.unwrap());
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024]; // cannot handle arbitrary size requests.
-    stream.read(&mut buffer).unwrap();
-    println!("{}", String::from_utf8_lossy(&buffer));
+fn handle_http_request(mut stream: TcpStream) {
+    let mut request_buffer = [0u8; 1024];
+    stream.read(&mut request_buffer).unwrap(); // TODO handle error
+    let request_buffer = String::from_utf8_lossy(&request_buffer);
+    println!("{}", request_buffer);
 
-    let file_string = std::fs::read_to_string("public/index.html").unwrap();
+    // TODO handle arbitrary GET requests
+    let response = if request_buffer.starts_with("GET / HTTP/1.1\r\n") {
+        get_page(&"public/index.html".to_string(), 200)
+    } else {
+        get_page(&"public/404.html".to_string(), 404)
+    };
+
+    println!("{}", response);
+    stream.write(response.as_bytes()).unwrap(); // TODO handle error
+    stream.flush().unwrap(); // TODO handle error
+}
+
+fn get_page(filepath: &String, status: u32) -> String {
+    let file_string = std::fs::read_to_string(filepath).unwrap(); // TODO handle error
     let response = format!(
-        "HTTP/1.1 200 OK\r\n \
-        Content-Length: {}\r\n\r\n \
-        {}", file_string.len(), file_string
+        "HTTP/1.1 {} _\r\n\
+        Content-Length: {}\r\n\r\n\
+        {}", status, file_string.len(), file_string
     );
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    return response;
 }
