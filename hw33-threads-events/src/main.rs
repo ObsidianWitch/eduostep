16dae13de714bd05f377a984762d4acc42ccf7a2
Obsidienne <obsidienne@protonmail.com>
2021-07-22 15:55:53 +0200

    hw33: reading request

--- hw33-threads-events/src/main.rs
+++ hw33-threads-events/src/main.rs
@@ -1,3 +1,4 @@
+use std::io::prelude::Read;
 use std::net::{TcpListener, TcpStream};
 
 // ref: https://doc.rust-lang.org/book/ch20-00-final-project-a-web-server.html
@@ -5,8 +6,13 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:0").unwrap();
     println!("TCP listener on {}", listener.local_addr().unwrap());
 
-    for _stream in listener.incoming() {
-        let stream = stream.unwrap();
-        println!("Connection established.");
+    for stream in listener.incoming() {
+        handle_connection(stream.unwrap());
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024]; // cannot handle arbitrary size requests.
+    stream.read(&mut buffer).unwrap();
+    println!("{}", String::from_utf8_lossy(&buffer));
 }
