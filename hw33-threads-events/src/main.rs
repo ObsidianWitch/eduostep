d846bad374f7743cc94f549333c2708af9b20870
Obsidienne <obsidienne@protonmail.com>
2021-07-25 01:13:10 +0200

    hw33: BufReader

--- hw33-threads-events/src/main.rs
+++ hw33-threads-events/src/main.rs
@@ -1,5 +1,6 @@
-use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::io::{BufRead, BufReader, Write};
+use std::time::Duration;
 
 // ref: https://doc.rust-lang.org/book/ch20-00-final-project-a-web-server.html
 fn main() {
@@ -12,29 +13,32 @@ fn main() {
 }
 
 fn handle_http_request(mut stream: TcpStream) {
-    let mut request_buffer = [0u8; 1024];
-    stream.read(&mut request_buffer).unwrap(); // TODO handle error
-    let request_buffer = String::from_utf8_lossy(&request_buffer);
-    println!("{}", request_buffer);
+    stream.set_read_timeout(Some(Duration::new(30, 0))).unwrap();
+    stream.set_write_timeout(Some(Duration::new(30, 0))).unwrap();
+
+    let mut reader = BufReader::new(&stream);
+    let mut status_line = String::new();
+    reader.read_line(&mut status_line).unwrap(); // TODO handle error
+    println!("{}", status_line);
 
     // TODO handle arbitrary GET requests
-    let response = if request_buffer.starts_with("GET / HTTP/1.1\r\n") {
+    let response = if status_line.starts_with("GET / HTTP/1.1\r\n") {
         get_page(&"public/index.html".to_string(), 200)
     } else {
         get_page(&"public/404.html".to_string(), 404)
     };
-
     println!("{}", response);
-    stream.write(response.as_bytes()).unwrap(); // TODO handle error
-    stream.flush().unwrap(); // TODO handle error
+    println!("---");
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
 
 fn get_page(filepath: &String, status: u32) -> String {
     let body = std::fs::read_to_string(filepath).unwrap(); // TODO handle error
-    return get_response(status, &body);
+    return new_response(status, &body);
 }
 
-fn get_response(status: u32, body: &String) -> String {
+fn new_response(status: u32, body: &String) -> String {
     return format!(
         "HTTP/1.1 {} _\r\nContent-Length: {}\r\n\r\n{}",
         status, body.len(), body
