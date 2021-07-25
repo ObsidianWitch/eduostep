d88fb38ef04b44bf57bd2431df8481888b721b03
Obsidienne <obsidienne@protonmail.com>
2021-07-25 04:07:40 +0200

    hw33: handle read_line() error

--- hw33-threads-events/src/main.rs
+++ hw33-threads-events/src/main.rs
@@ -1,5 +1,5 @@
-use std::net::{TcpListener, TcpStream};
 use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
 use std::time::Duration;
 
 // ref: https://doc.rust-lang.org/book/ch20-00-final-project-a-web-server.html
@@ -18,32 +18,37 @@ fn handle_http_request(mut stream: TcpStream) {
 
     let mut reader = BufReader::new(&stream);
     let mut status_line = String::new();
-    reader.read_line(&mut status_line).unwrap(); // TODO handle error
-    println!("{}", status_line);
-    let status_line: Vec<&str> = status_line.split(' ').collect();
-
-    let response = if status_line.get(0) != Some(&"GET") {
-        get_error_page(501, "Not Implemented")
-    } else if status_line.get(2) != Some(&"HTTP/1.1\r\n") {
-        get_error_page(400, "Bad Request")
-    } else if let Some(path) = status_line.get(1) {
-        get_page(path).unwrap_or_else(|_e| get_error_page(404, "Not Found"))
+    let response = if let Err(_) = reader.read_line(&mut status_line) {
+        get_error_page(500, "Internal Server Error")
     } else {
-        get_error_page(400, "Bad Request")
+        println!("{}", status_line);
+        let status_line: Vec<&str> = status_line.split(' ').collect();
+        if status_line.get(0) != Some(&"GET") {
+            get_error_page(501, "Not Implemented")
+        } else if status_line.get(2) != Some(&"HTTP/1.1\r\n") {
+            get_error_page(400, "Bad Request")
+        } else if let Some(path) = status_line.get(1) {
+            get_page(path).unwrap_or_else(|_|
+                get_error_page(404, "Not Found")
+            )
+        } else {
+            get_error_page(400, "Bad Request")
+        }
     };
-    println!("{}", response);
-    println!("---");
+    println!("{}\n---", response);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
 fn get_error_page<Str: AsRef<str>>(status: u32, reason: Str) -> String {
     let reason = reason.as_ref();
-    let body = format!("<!DOCTYPE html>
+    let body = format!(
+        "<!DOCTYPE html>
         <html lang=\"en\">
         <head><meta charset=\"utf-8\"><title>{0} {1}</title></head>
         <body><h1>{0} {1}</h1></body>
-        </html>", status, reason);
+        </html>", status, reason
+    );
     return new_response(status, reason, &body);
 }
 
