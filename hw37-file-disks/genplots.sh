3b7832dfacec33df8f66dc621071ab18dc25f14a
Obsidienne <obsidienne@protonmail.com>
2021-08-03 03:28:34 +0200

    hw37: q2 plot

--- hw37-file-disks/genplots.sh
+++ hw37-file-disks/genplots.sh
@@ -4,6 +4,17 @@ set -o errexit -o nounset
 
 time {
     for addr in '7,30,8' '10,11,12,13'; do
+    for seekspeed in $(seq 0.1 0.1 5.0); do
+        # note: some results are discarded because seek speed must divide
+        # evenly into track width
+        ./disk.py -a "$addr" -S "$seekspeed" -c \
+            | awk '/addr /{a=$3} /seekSpeed/{input=$3} \
+                   /TOTALS/{print a, "SeekSpeed:"input, $0}'
+    done; done | ./plot.py > plot_q2.png
+}
+
+time {
+    for addr in '7,30,8' '10,11,12,13'; do
     for rotspeed in $(seq 0.1 0.1 5.0); do
         ./disk.py -a "$addr" -R "$rotspeed" -c \
             | awk '/addr /{a=$3} /rotateSpeed/{input=$3} \
