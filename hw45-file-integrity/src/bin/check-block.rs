ae7fea4d1800c206a659014b919d4fb2df8d9ac0
Obsidienne <obsidienne@protonmail.com>
2021-09-17 05:29:57 +0200

    hw45:check-block: create()

--- hw45-file-integrity/src/bin/check-block.rs
+++ hw45-file-integrity/src/bin/check-block.rs
@@ -1,10 +1,15 @@
 use hw45::xor8sum;
 
+fn create(in_data: &str, out_checksums: &str) -> std::io::Result<()> {
+    let data = std::fs::read(in_data)?;
+    let checksums: Vec<u8> = data.chunks(4096).map(xor8sum).collect();
+    std::fs::write(out_checksums, checksums)?;
+    return Ok(());
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let path_in = args.get(1).expect("missing operand: in_data_path");
     let path_out = args.get(2).expect("missing operand: out_checksums_path");
-    let data = std::fs::read(path_in).unwrap();
-    let checksums: Vec<u8> = data.chunks(4096).map(|chunk| xor8sum(chunk)).collect();
-    std::fs::write(path_out, checksums).unwrap();
+    create(path_in, path_out).unwrap();
 }
