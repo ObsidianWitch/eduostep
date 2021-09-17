ad5475c5be4898df4dfb217ffb395d6560691935
Obsidienne <obsidienne@protonmail.com>
2021-09-17 04:58:35 +0200

    hw45:lib: {xor8,fletcher8,crc16}sum(IntoIterator) (old: Iterator)

--- hw45-file-integrity/src/bin/check-xor.rs
+++ hw45-file-integrity/src/bin/check-xor.rs
@@ -4,6 +4,6 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let path = args.get(1).expect("missing operand: path");
     let data = std::fs::read(path).unwrap(); // no buffering for simplicity
-    let checksum = xor8sum(data.iter());
+    let checksum = xor8sum(&data);
     println!("xor8sum {} {:#04x} {:#010b}", checksum, checksum, checksum);
 }
