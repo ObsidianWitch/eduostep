e8fa94c1107cd7ad9c0da31b80d97897ea3e04db
Obsidienne <obsidienne@protonmail.com>
2021-09-17 05:47:02 +0200

    hw45:check-block: check()

--- hw45-file-integrity/src/bin/check-block.rs
+++ hw45-file-integrity/src/bin/check-block.rs
@@ -7,9 +7,22 @@ fn create(in_data: &str, out_checksums: &str) -> std::io::Result<()> {
     return Ok(());
 }
 
+fn check(in_data: &str, in_checksums: &str) -> std::io::Result<()> {
+    let data = std::fs::read(in_data)?;
+    let computed_checksums: Vec<u8> = data.chunks(4096).map(xor8sum).collect();
+    let saved_checksums = std::fs::read(in_checksums)?;
+    assert!(computed_checksums == saved_checksums);
+    return Ok(());
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let path_in = args.get(1).expect("missing operand: in_data_path");
-    let path_out = args.get(2).expect("missing operand: out_checksums_path");
-    create(path_in, path_out).unwrap();
+    let operation = args.get(1).expect("missing operand: op:create|check");
+    let data_path = args.get(2).expect("missing operand: data_path");
+    let checksums_path = args.get(3).expect("missing operand: checksums_path");
+    match operation.as_str() {
+        "create" => create(data_path, checksums_path).unwrap(),
+        "check" => check(data_path, checksums_path).unwrap(),
+        _ => panic!("incorrect operand: op:create|check"),
+    }
 }
